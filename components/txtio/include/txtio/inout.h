a452dd1765ed469cccafa5a3cb2d4b6db581f566
zwiebert <bertw@online.de>
2020-04-26 19:12:28 +0200

    Code clean up

--- components/txtio/include/txtio/inout.h
+++ components/txtio/include/txtio/inout.h
@@ -47,20 +47,9 @@ void io_putd(int n);
 void io_putld(int32_t n);
 void io_putx8(uint8_t n);
 
-
-
 void printBCD(uint8_t bcd);
 void print_array_8(const uint8_t *src, int len);
 void print_array_8_inv(const uint8_t *src, int len);
-
-bool mcu_get_buttonUpPin(void);
-bool mcu_get_buttonDownPin(void);
-bool mcu_get_buttonPin(void);
-
-typedef enum mcu_pin_state { PIN_DEFAULT=0, PIN_INPUT, PIN_INPUT_PULLUP, PIN_OUTPUT, PIN_ERROR, PIN_READ, PIN_CLEAR, PIN_SET, PIN_TOGGLE, PIN_INPUT_OUTPUT } mcu_pin_state;
-const char* mcu_access_pin(int gpio_number, mcu_pin_state *result, mcu_pin_state state);
-bool  is_gpio_number_usable(int gpio_number, bool cli);
-void gpio_get_levels(unsigned long long gpio_mask, char *buf, int buf_size);
 
 #ifdef TEST_HOST
 #include <stdio.h>
