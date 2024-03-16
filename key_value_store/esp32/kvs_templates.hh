7f1695dcb39c15ceeb803f4b5cddb786392f4f8d
zwiebert <bertw@online.de>
2024-03-16 10:40:39 +0100

    Fix build error

--- key_value_store/esp32/kvs_templates.hh
+++ key_value_store/esp32/kvs_templates.hh
@@ -4,8 +4,9 @@
 
 #include "nvs.h"
 
-template<typename cmp_fun_type, typename cmp_arg_type, typename cb_fun_type, typename cb_arg_type>
-int kvs_foreach(const char *name_space, kvs_type_t type, cmp_fun_type cmp, cmp_arg_type &&cmp_arg, cb_fun_type cb, cb_arg_type &&cb_arg) {
+/// just count matching keys. no callback
+template<typename cmp_fun_type, typename cmp_arg_type>
+int kvs_foreach(const char *name_space, kvs_type_t type, cmp_fun_type cmp, cmp_arg_type &&cmp_arg) {
   nvs_iterator_t it = NULL;
   esp_err_t res = nvs_entry_find(CFG_PARTNAME, name_space, (nvs_type_t) type, &it);
 
@@ -18,22 +19,39 @@ int kvs_foreach(const char *name_space, kvs_type_t type, cmp_fun_type cmp, cmp_a
     if (cmp && !cmp(info.key, cmp_arg))
       continue;
 
-    if (cb) {
-      switch (cb(info.key, (kvs_type_t) info.type, cb_arg)) {
-      case kvsCb_match:
-        ++count;
-        break;
-      case kvsCb_done:
-        ++count;
-        return count;
-      case kvsCb_noMatch:
-        break;
-      }
-    } else {
-      ++count;
-    }
+    ++count;
   }
   return count;
 }
 
+template<typename cmp_fun_type, typename cmp_arg_type, typename cb_fun_type, typename cb_arg_type>
+int kvs_foreach(const char *name_space, kvs_type_t type, cmp_fun_type cmp, cmp_arg_type &&cmp_arg, cb_fun_type cb, cb_arg_type &&cb_arg) {
+  assert(cb);
+
+  nvs_iterator_t it = NULL;
+  esp_err_t res = nvs_entry_find(CFG_PARTNAME, name_space, (nvs_type_t) type, &it);
+
+  int count = 0;
+  while (res == ESP_OK) {
+    nvs_entry_info_t info;
+    nvs_entry_info(it, &info);
+    res = nvs_entry_next(&it);
+
+    if (cmp && !cmp(info.key, cmp_arg))
+      continue;
+
+    switch (cb(info.key, (kvs_type_t) info.type, cb_arg)) {
+    case kvsCb_match:
+      ++count;
+      break;
+    case kvsCb_done:
+      ++count;
+      return count;
+    case kvsCb_noMatch:
+      break;
+    }
+
+  }
+  return count;
+}
 
--- key_value_store/host/kvs_templates.hh
+++ key_value_store/host/kvs_templates.hh
@@ -41,6 +41,37 @@ static struct kvs_handle handles[HANDLE_COUNT];
 int kvs_find_next(kvshT h, struct line_info *li, int pos, const char *key, kvs_type_t kvs_type);
 bool kvs_checkMagicCookie(struct line_info *lip, int pos);
 
+/// just count matching keys. no callback
+template<typename cmp_fun_type, typename cmp_arg_type>
+int kvs_foreach(const char *name_space, kvs_type_t type, cmp_fun_type cmp, cmp_arg_type &&cmp_arg) {
+
+  int pos = 0;
+  kvshT h = 0;
+  int count = 0;
+
+  if ((h = kvs_open(name_space, kvs_READ))) {
+    struct line_info li = { COOKIE };
+    while ((pos = kvs_find_next(h, &li, pos, 0, type)) >= 0) {
+      if (!li_isUsed(&li))
+        continue;
+
+      if (!kvs_checkMagicCookie(&li, pos)) {
+        return ERR_CORRUPT_FILE;
+      }
+
+      if (cmp && !cmp(li.key, cmp_arg))
+        continue;
+
+      ++count;
+    }
+    goto succ;
+  }
+
+  succ: if (h)
+    kvs_close(h);
+  return count;
+}
+
 template<typename cmp_fun_type, typename cmp_arg_type, typename cb_fun_type, typename cb_arg_type>
 int kvs_foreach(const char *name_space, kvs_type_t type, cmp_fun_type cmp, cmp_arg_type &&cmp_arg, cb_fun_type cb, cb_arg_type &&cb_arg) {
 
